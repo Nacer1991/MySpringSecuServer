8345b3f8e5cc53bee6c270964dbffcd0f3502356
abdounes <abdoune.nacer91@gmail.com>
2018-12-25 00:59:44 +0100

    first commit

---
+++ src/main/java/com/nacer/MySpringSecuApplication.java
@@ -0,0 +1,14 @@
+package com.nacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringSecuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringSecuApplication.class, args);
+	}
+
+}
+
